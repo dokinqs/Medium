6502e7e722f31998760d499c58fdfaf5db9d01fd
Karen Zhao <karen.zhao18@gmail.com>
2020-02-10 11:51:12 -0500

    add modal

---
+++ index.js
@@ -0,0 +1,46 @@
+$(window).scroll(
+  // anonymous JS object to store the value
+    {
+        previousTop: 0
+    }, 
+    () => {
+      let currentTop = $(window).scrollTop();
+      if (currentTop < this.previousTop) {
+        // scrolled up
+          $("#navbar").show();
+          // $("#navbar").slideUp(300);
+      } else {
+        // scrolled down
+          $("#navbar").hide();
+          // $("#navbar").slideDown(300);
+      }
+      this.previousTop = currentTop;
+});
+
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("follow");
+let xbtn = document.getElementsByClassName("close")[0];
+
+// OPEN modal 
+
+//  clicking follow button
+btn.onclick = function() {
+  modal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+}
+
+// CLOSE modal
+
+//  clicking <span> (x) 
+xbtn.onclick = function() {
+  modal.style.display = "none";
+  document.body.style.overflowY = "auto";
+}
+//  clicking outside of the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.body.style.overflowY = "auto";
+  }
+}
