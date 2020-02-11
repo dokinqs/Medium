cb1beacf20c45331b4b4679fbbaf07e3e30de86c
Karen Zhao <karen.zhao18@gmail.com>
2020-02-11 17:14:34 -0500

    like hover anim, likes modal

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// NAV - hide on scroll down
+
 $(window).scroll(
   // anonymous JS object to store the value
     {
@@ -18,29 +20,71 @@ $(window).scroll(
 });
 
 
+// FOLLOW MODAL
+
 let modal = document.getElementById("myModal");
-let btn = document.getElementById("follow");
+let btns = document.getElementsByClassName("follow");
 let xbtn = document.getElementsByClassName("close")[0];
 
-// OPEN modal 
+// Open modal: 
 
 //  clicking follow button
-btn.onclick = function() {
-  modal.style.display = "block";
-  document.body.style.overflowY = "hidden";
-}
+Object.entries(btns).map( (obj) => {
+    // obj = Array[index, object] (object is the HTML element object). the actual element is stored in obj[1], not object
+    obj[1].addEventListener("click", function() {
+        modal.style.display = "block";
+        document.body.style.overflowY = "hidden";
+    });
+})
 
-// CLOSE modal
+// Close modal:
 
 //  clicking <span> (x) 
 xbtn.onclick = function() {
   modal.style.display = "none";
   document.body.style.overflowY = "auto";
 }
-//  clicking outside of the modal
+//  pressing esc button on laptop
+$(document).keyup( (e) => {
+    if (e.keyCode === 27) {
+        modal.style.display = "none";
+        document.body.style.overflowY = "auto";
+    }
+})
+
+
+// LIKES MODAL
+
+let likesmodal = document.getElementById("likesModal");
+let btn = document.getElementsByClassName("likes")[0];
+let closebtn = document.getElementsByClassName("close")[1];
+
+// Open
+btn.onclick = function() {
+  likesmodal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+}
+
+// Close
+closebtn.onclick = function() {
+  likesmodal.style.display = "none";
+  document.body.style.overflowY = "auto";
+}
+$(document).keyup( (e) => {
+    if (e.keyCode === 27) {
+        likesmodal.style.display = "none";
+        document.body.style.overflowY = "auto";
+    }
+})
+
+//  clicking outside of the modal for both modals
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == likesmodal) {
+    likesmodal.style.display = "none";
+    document.body.style.overflowY = "auto";
+  } else if (event.target == modal) {
     modal.style.display = "none";
     document.body.style.overflowY = "auto";
   }
 }
+
